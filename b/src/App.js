fdf3c37a6d692167f7a9c77d4878eebbcccabceb
dantistas <rytisvenslovas@gmail.com>
2020-11-07 14:14:10 +0100

    8.8

--- b/src/App.js
+++ b/src/App.js
@@ -4,8 +4,37 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
+import { gql, useQuery } from '@apollo/client'
+
+
+const ALL_AUTHORS = gql`
+
+query{
+  allAuthors{
+    name
+    born
+    bookCount
+    id
+  }
+}
+
+`
+
+
 const App = () => {
   const [page, setPage] = useState('authors')
+
+
+  const result = useQuery(ALL_AUTHORS)
+
+
+
+  if(result.loading){
+    return (
+      <div>loading...</div>
+    )
+  }
+
 
   return (
     <div>
@@ -15,7 +44,7 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors
+      <Authors authors={result.data.allAuthors}
         show={page === 'authors'}
       />
 
