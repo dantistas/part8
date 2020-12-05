74dc7e53b8da84137fe1d5801d11647dec633911
rytis <rytisvenslovas@gmail.com>
2020-12-05 10:57:06 +0000

    8.19

--- a/index.js
+++ a/index.js
@@ -107,9 +107,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
 
 
 const typeDefs = gql`
-
 type Mutation {
-
   createUser(
     username: String!
     favoriteGenre: String!
@@ -118,60 +116,45 @@ type Mutation {
     username: String!
     password: String!
   ): Token
-
   addBook(
     title: String!
     author: String!
     published: Int!
     genres: [String!]
   ): Book
-
   editAuthor(
     name: String!
     setBornTo:Int!
   ): Author
 }
-
 type Book {
-
     title: String!
     published: Int!
     author: Author!                  
     id: ID!
     genres: [String]
-
 }
-
 type Author {
-
   name: String
   id: ID
   born: Int
   bookCount: Int
-
 }
-
 type User {
   username: String!
   favoriteGenre: String!
   id: ID!
 }
-
 type Token {
   value: String!
 }
-
   type Query {
-
     me: User  
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String ): [Book!]!
     allAuthors: [Author!]!  
-
   }
-
-
 `
 
 
@@ -232,6 +215,7 @@ const resolvers = {
 
     addBook: async (root, args, context)  => {
       console.log("args: ",args)
+      console.log("useris: ",context.currentUser)
       if(!context.currentUser){
         throw new AuthenticationError("not authenticated")
       }else {
--- b/src/components/Books.js
+++ b/src/components/Books.js
@@ -1,37 +1,77 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Books = (props) => {
+  const [books, setBooks] = useState([])
+
+  const genres = props.books.map(b => b.genres.map(genre => genre))
+
+  let genreButtons = []
+
+  for(let i = 0; i<genres.length; i++){
+    genreButtons.push(...genres[i]) 
+}
+
+for(let i = 0 ; i < genreButtons.length ; i++ ){
+  for(let j = 0; j<genreButtons.length; j++ ){
+    if(genreButtons[i] === (genreButtons[j]) && i !== j){
+      genreButtons.splice(genreButtons.indexOf(genreButtons[j]),1)
+    }
+  }
+}
+
+  
+  
+  useEffect(()=>{
+    setBooks(props.books)
+  },[props.books])
+  
+const filterByGenre = (genre) => {
+  setBooks(props.books.filter((b)=>{
+    if(b.genres.indexOf(genre)>= 0){
+      return b
+    }
+  }))
+}
+
+
+
+
+
+
+
   if (!props.show) {
     return null
   }
 
-  const books = props.books
-
+  
   return (
     <div>
-      <h2>books</h2>
-      <button onClick={()=>{console.log(books)}}>booksai</button>
-
+      <h2>Books</h2>
+      <button onClick={()=>{console.log(genreButtons)}}>genrebuttonms</button>
       <table>
         <tbody>
           <tr>
-            <th></th>
-            <th>
-              author
+            <th> 
+              Title
             </th>
             <th>
-              published
+              Author
+            </th>
+            <th>
+              Published
             </th>
           </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {books.map(b =>
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author.name}</td>
+              <td>{b.published}</td>
             </tr>
           )}
         </tbody>
       </table>
+          <h1>Filter by genre: </h1><button onClick={()=>{setBooks(props.books)}}>All</button> {genreButtons.map(genre => <button onClick={()=>{filterByGenre(genre)}}>{genre}</button>)}
+
     </div>
   )
 }
